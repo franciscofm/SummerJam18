16b484fd69f8433c7221d0b4a205f306158879e8
Fran <franciscofernandezmanyas@gmail.com>
2018-08-10 20:15:07 +0200

    Start management

---
+++ Assets/Game_Management/Building.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Building : MonoBehaviour, IPointerDownHandler {
+
+	public GameObject menuPrefab;
+	public enum Side {
+		Left,
+		Right
+	};
+	public Side side;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnPointerDown(PointerEventData data) {
+		Menus.instance.OpenMenu (side == Side.Left, menuPrefab);
+	}
+
+}
---
+++ Assets/Game_Management/Menus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menus : MonoBehaviour {
+
+	public static Menus instance;
+
+	public RectTransform leftPanel, rightPanel;
+	public RectTransform barPanel;
+
+	public Vector2 leftHide, leftShow;
+	public Vector2 rightHide, rightShow;
+	public Vector2 barHide, barShow;
+
+	// Use this for initialization
+	void Awake() {
+		instance = this;
+	}
+
+	void Start () {
+		Canvas.ForceUpdateCanvases ();
+
+		leftHide = leftShow = leftPanel.position;
+		leftHide.x -= leftPanel.rect.width;
+		leftPanel.position = leftHide;
+
+		rightHide = rightShow = rightPanel.position;
+		rightHide.x += rightPanel.rect.width;
+		rightPanel.position = rightHide;
+
+		barHide = barShow = barPanel.position;
+		barHide.y += barPanel.rect.height * 0.5f;
+		barPanel.position = barHide;
+
+	}
+
+	public void OpenMenu(bool left, GameObject menu) {
+		RectTransform target = left ? leftPanel : rightPanel;
+
+		if (target.childCount != 0) 
+			Destroy (target.GetChild (0).gameObject);
+
+
+	}
+}
